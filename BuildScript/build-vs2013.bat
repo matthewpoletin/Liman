20f0a6abe81b50dcf8dd8093213f6818bda68461
matthewpoletin <contact@matthewpoletin.ru>
2015-08-17 18:07:02 +0300

    vs build bat's

---
+++ BuildScript/build-vs2013.bat
@@ -0,0 +1,3 @@
+CLS
+premake4 clean
+premake5 vs2013 --file=premake4.lua --os=windows
--- BuildScript/build-vs2015.bat
+++ BuildScript/build-vs2015.bat
@@ -1,5 +1,3 @@
 CLS
 premake4 clean
-REM premake5 xcode4 --file=premake4.lua --os=macos
-REM premake5 codeblocks --file=premake4.lua --os=linux
 premake5 vs2015 --file=premake4.lua --os=windows
